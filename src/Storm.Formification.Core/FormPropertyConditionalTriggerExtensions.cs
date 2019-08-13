5d59d62d15dc76cc7386a46fba99cce47129b1dd
Phil Oyston <philip.oyston@stormid.com>
2019-08-13 15:37:52 +0100

    Adds wip implementation of boolean based conditional triggers

---
+++ src/Storm.Formification.Core/FormPropertyConditionalTriggerExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Storm.Formification.Core
+{
+    public static class FormPropertyConditionalTriggerExtensions
+    {
+        public static void SetConditionalTriggerTarget(this FormProperty formProperty, string triggerKey)
+        {
+            formProperty.Metadata.Add("_trigger-target", triggerKey);
+        }
+
+        public static string GetConditionalTriggerTarget(this FormProperty formProperty)
+        {
+            if (formProperty.Metadata.TryGetValue("_trigger-target", out var triggerTargetValue) && triggerTargetValue is string triggerTarget)
+            {
+                return triggerTarget;
+            }
+
+            return null;
+        }
+
+        public static void SetConditionalTrigger(this FormProperty formProperty, string triggerKey)
+        {
+            formProperty.Metadata.Add("_trigger", triggerKey);
+        }
+
+        public static string GetConditionalTrigger(this FormProperty formProperty)
+        {
+            if(formProperty.Metadata.TryGetValue("_trigger", out var triggerKeyValue) && triggerKeyValue is string triggerKey)
+            {
+                return triggerKey;
+            }
+
+            return null;
+        }
+    }
+}
